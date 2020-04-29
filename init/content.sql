9c652e5e407e42c55e72aec126f097de28218766
KodSeven DEV <kod7dev@gmail.com>
2020-04-29 05:22:36 +0300

    content sql güncellendi

--- init/content.sql
+++ init/content.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: localhost
--- Üretim Zamanı: 29 Nis 2020, 00:29:43
+-- Üretim Zamanı: 29 Nis 2020, 04:22:02
 -- Sunucu sürümü: 10.4.11-MariaDB
 -- PHP Sürümü: 7.4.4
 
@@ -39,10 +39,18 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `name`, `author`, `cover`) VALUES
-(1, 'Son Av', 'Jean Christophe Grange', '1.jpg'),
-(2, 'Gelirken Ekmek Al', 'Şermin Yaşar', '2.jpg'),
-(3, 'Çocuk Yetiştirme Kılavuzu', 'Selçuk R. Şirin', '3.jpg'),
-(4, 'Beni Ödülle Cezalandırma', 'Dr. Özgür Bolat', '4.jpg');
+(19, 'Bir Çöküşün Öyküsü', 'Stefan Zweig', 'bic.jpg'),
+(20, 'Ay Işığı Sokağı', 'Stefan Zweig', 'AYISIGI.jpg'),
+(21, 'Korku', 'Stefan Zweig', 'KORKU.jpg'),
+(22, 'Zacharius Usta', 'Jules Verne', 'zachar.jpg'),
+(23, 'Muhteşem Gatsby', 'Francis Scott Fitzgerald', 'MUHTESEM.jpg'),
+(24, 'Martı', 'Anton Pavloviç Çehov', 'marti.jpg'),
+(25, 'Köpek Kalbi', 'Mihail Bulgakov', 'kopek.jpg'),
+(26, 'Hayatım – Bir Taşralının Hikâyesi', 'Anton Pavloviç Çehov', 'hayatim.jpg'),
+(27, 'Usta ve Margarita', ' Mihail Bulgakov', 'ustave.jpg'),
+(28, 'Ölümcül Yumurtalar', 'Mihail Bulgakov', 'OLUMCUL.jpg'),
+(29, 'Yıldız Gezgini', 'Jack London', 'YILDIZ-GEZGINI.jpg'),
+(30, 'Ateş Yakmak', 'Jack London', 'ates.jpg');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -62,7 +70,7 @@ ALTER TABLE `books`
 -- Tablo için AUTO_INCREMENT değeri `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
